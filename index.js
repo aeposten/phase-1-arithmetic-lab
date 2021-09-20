1c48cb8aba6e3ab94504d78ab70fca0c18caabd4
Amy Posten <amyposten85@gmail.com>
2021-09-20 16:24:36 -0400

    commit tree 5f748d34cdf01df76b01374571c297421a7e44ad

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random()*10);
+
+let num3 = 19;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let num5 = 10;
+let num6 = 20;
+
+let max = Math.max(num5, num6);
